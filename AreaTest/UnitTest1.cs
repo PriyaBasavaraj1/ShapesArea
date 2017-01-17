98b54e75b0f756bb85596f2b42a116ed34f354b9
PriyaBasavaraj <priya.basavaraj.03@gmail.com>
2017-01-17 15:41:16 +0530

    Calculating Area of Different shapes

---
+++ AreaTest/UnitTest1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShapesArea;
+
+namespace AreaTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ValidInputs_for_AreaOfCircle_DoesnotThrowAnException()
+        {
+            var shapeUndertest = ShapeFactory.GetShape("Circle");
+            double res = shapeUndertest.CalculateArea(3).Display();
+            Assert.AreEqual(Math.PI * 3 * 3, res, "error");
+        }
+
+        [TestMethod]
+        public void ValidInputs_for_AreaOfRectangle_DoesnotThrowAnException()
+        {
+            var shapeUndertest = ShapeFactory.GetShape("Rectangle");
+            double res = shapeUndertest.CalculateArea(3,4).Display();
+            Assert.AreEqual(3 * 4, res, "error");
+        }
+
+        [TestMethod]
+        public void ValidInputs_for_AreaOfTriangle_DoesnotThrowAnException()
+        {
+            var shapeUndertest = ShapeFactory.GetShape("Triangle");
+            double res = shapeUndertest.CalculateArea(3,4).Display();
+            Assert.AreEqual(0.5 * 3 * 4, res, "error");
+        }
+
+        [TestMethod,ExpectedException(typeof(Exception))]
+        public void InvalidChoice_ThrowsAnException()
+        {
+            var shapeUndertest = ShapeFactory.GetShape("ydhcv");
+            double res = shapeUndertest.CalculateArea(3).Display();
+        }
+
+        [TestMethod, ExpectedException(typeof(Exception))]
+        public void InvalidNumberOfParameters_for_AreaOfCircle_ThrowsAnException()
+        {
+            var shapeUndertest = ShapeFactory.GetShape("Circle");
+            double res = shapeUndertest.CalculateArea().Display();
+        }
+
+        [TestMethod, ExpectedException(typeof(Exception))]
+        public void InvalidNumberOfParameters_for_AreaOfRectangle_ThrowsAnException()
+        {
+            var shapeUndertest = ShapeFactory.GetShape("Rectangle");
+            double res = shapeUndertest.CalculateArea(3,4,5).Display();
+        }
+
+        [TestMethod, ExpectedException(typeof(Exception))]
+        public void InvalidNumberOfParameters_for_AreaOfTriangle_ThrowsAnException()
+        {
+            var shapeUndertest = ShapeFactory.GetShape("Triangle");
+            double res = shapeUndertest.CalculateArea(3, 4, 5).Display();
+        }
+
+        [TestMethod, ExpectedException(typeof(Exception))]
+        public void InvalidRange_for_AreaOfCircle_ThrowsAnException()
+        {
+            var shapeUndertest = ShapeFactory.GetShape("Circle");
+            double res = shapeUndertest.CalculateArea(-3).Display();
+        }
+
+        [TestMethod, ExpectedException(typeof(Exception))]
+        public void InvalidRange_for_AreaOfRectangle_ThrowsAnException()
+        {
+            var shapeUndertest = ShapeFactory.GetShape("Rectangle");
+            double res = shapeUndertest.CalculateArea(2,-3).Display();
+        }
+
+        [TestMethod, ExpectedException(typeof(Exception))]
+        public void InvalidRange_for_AreaOfTriangle_ThrowsAnException()
+        {
+            var shapeUndertest = ShapeFactory.GetShape("Triangle");
+            double res = shapeUndertest.CalculateArea(-3,6).Display();
+        }
+    }
+}
---
+++ ShapesArea/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapesArea
+{
+    public abstract class Shape
+    {
+        public double result;
+        public abstract Shape CalculateArea(params double[] vals);
+        
+        public double Display()
+        {
+            return result;
+        }
+    }
+
+    //Area Calculation for Circle
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+        public override Shape CalculateArea(params double[] vals)
+        {
+            if (vals.Length != 1)
+                throw new Exception("Enter one value");
+            if (vals[0] <= 0 || vals[0] > double.MaxValue)
+                throw new Exception("Enter the value within the range of double");
+            else
+            {
+                Radius = vals[0];
+                result = Math.PI * Radius * Radius;
+            }
+            return this;
+        }
+    }
+
+    //Area Calculation for Rectangle
+    public class Rectangle : Shape
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public override Shape CalculateArea(params double[] vals)
+        {
+            if (vals.Length != 2)
+                throw new Exception("Enter two value");
+            if ((vals[0] <= 0 || vals[0] > double.MaxValue) || (vals[1] <= 0 || vals[1] > double.MaxValue))
+                throw new Exception("Enter the value within the range of double");
+            else
+            {
+                Length = vals[0];
+                Width = vals[1];
+                result = vals[0] * vals[1];
+            }
+            return this;
+        }
+    }
+
+    //Area Calculation for Triangle
+    public class Triangle : Shape
+    {
+        public double Base1 { get; set; }
+        public double Height { get; set; }
+
+        public override Shape CalculateArea(params double[] vals)
+        {
+            if (vals.Length != 2)
+                throw new Exception("Enter two values");
+            if ((vals[0] <= 0 || vals[0] > double.MaxValue) || (vals[1] <= 0 || vals[1] > double.MaxValue))
+                throw new Exception("Enter the value within the range of double");
+            else
+            {
+                Base1 = vals[0];
+                Height = vals[1]; 
+                result = 0.5 * vals[0] * vals[1];
+            }
+            return this;
+        }
+    }
+
+    //class to return an instance
+    public class ShapeFactory
+    {
+        public static Shape GetShape(string ch)
+        {
+            switch(ch)
+            {
+                case "Circle": return new Circle();
+                case "Rectangle": return new Rectangle();
+                case "Triangle": return new Triangle();
+                default:throw new Exception("Invalid Choice");
+            } 
+        }
+    }
+
+    //main program
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice, val;
+            while (true)
+            {
+                Console.WriteLine("\nEnter a choice\n * Circle\n * Rectangle\n * Triangle\n * Exit");
+                choice = Console.ReadLine();
+                if (choice == "Exit")
+                    Environment.Exit(0);
+
+                Shape shape = ShapeFactory.GetShape(choice);
+
+                Console.WriteLine("Enter the value :");
+                val = Console.ReadLine();
+                double[] values = Array.ConvertAll(val.Split(' '), Double.Parse);
+                Console.WriteLine("Area Is : {0}", shape.CalculateArea(values).Display());
+
+            }
+        }
+    }
+}
